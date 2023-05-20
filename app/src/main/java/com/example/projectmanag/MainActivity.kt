801de0ccb9aba7d21c0bcbb9eb0980dfa45839e7
NolvertoUrias <nuriasobeso@gmail.com>
2023-05-20 17:45:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/projectmanag/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectmanag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/projectmanag/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.projectmanag
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.projectmanag.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        /**
+         *  Adding a downloaded free font style
+         *  Link: https://www.1001fonts.com/carbon-font.html
+         */
+        val typeFace: Typeface = Typeface.createFromAsset(assets,"carbon bl.otf")
+        binding.tvAppName.typeface = typeFace
+    }
+}
